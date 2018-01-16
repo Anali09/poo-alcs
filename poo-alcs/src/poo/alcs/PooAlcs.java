3e8a4958075555cd7ad7c9e65ee4e27c1d5ea6a8
Anali09 <35473653+Anali09@users.noreply.github.com>
2018-01-15 19:41:27 -0600

    Update PooAlcs.java
    hola

--- poo-alcs/src/poo/alcs/PooAlcs.java
+++ poo-alcs/src/poo/alcs/PooAlcs.java
@@ -15,7 +15,7 @@ public class PooAlcs {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola mundo");
     }
     
 }
