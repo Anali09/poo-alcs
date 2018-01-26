7c98e2872c96f83f6542c6ee937a9091e943aa50
T-102 <T-102@PC243>
2018-01-25 19:47:28 -0600

    aplicaccion

--- poo-alcs/src/poo/alcs/Usuario.java
+++ poo-alcs/src/poo/alcs/Usuario.java
@@ -12,4 +12,20 @@ package poo.alcs;
 public class Usuario {
     float peso;
     float altura;
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
 }
