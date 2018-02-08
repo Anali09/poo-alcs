7a06c0dafb0d8d13512800eae9c038fddd0e75c1
T- <T-@PC040>
2018-02-07 21:46:03 -0600

    tarea

---
+++ poo-alcs/src/tarea1/Alumno.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea1;
+
+/**
+ *
+ * @author T-
+ */
+public class Alumno {
+    
+    public static void main(String[] args) {
+        
+        
+    }
+    int matricula;
+    String nombre;
+    String Paterno;
+    String Materno;
+    
+}
