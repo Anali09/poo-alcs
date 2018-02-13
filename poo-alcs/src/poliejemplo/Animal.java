4d474eeee797c4dcec467b210dfee29f3a9f9532
T-102 <T-102@PC242>
2018-02-12 19:47:00 -0600

    polimurfismo

---
+++ poo-alcs/src/poliejemplo/Animal.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poliejemplo;
+
+/**
+ *
+ * @author T-102
+ */
+public class Animal {
+    private String colo;
+    private int edad;
+
+    public String getColo() {
+        return colo;
+    }
+
+    public void setColo(String colo) {
+        this.colo = colo;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
