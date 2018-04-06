b239280d526fa30718af4a22fc12d89dfa36aeaa
T-102 <T-102@PC242>
2018-04-05 19:44:28 -0500

    Ventana

--- poo-alcs/src/Relojito/Ventanita.java
+++ poo-alcs/src/Relojito/Ventanita.java
@@ -3,34 +3,32 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package thr3ad1;
+package Relojito;
 
-import com.sun.istack.internal.logging.Logger;
 import java.time.LocalTime;
-import java.util.logging.Level;
 
 /**
  *
  * @author T-102
  */
-public class Relojito extends javax.swing.JFrame {
+public class Ventanita extends javax.swing.JFrame {
 
     /**
-     * Creates new form Relojito
+     * Creates new form Ventanita
      */
-    public Relojito() {
+    public Ventanita() {
         initComponents();
-         
-        Thread t1=new Thread(new Runnable (){
+        setSize(500,500);
+         Thread t1=new Thread(new Runnable (){
             @Override
             public void run() {
+                int decremento=0;
                while (true){
                    try {
-                        LocalTime tiempo=LocalTime.now();
-                        int hora= tiempo.getHour();
-                        int minuto = tiempo.getMinute();
-                        int segundo = tiempo.getSecond();
-                        jLabel1.setText(""+hora+";"+minuto+";"+segundo);
+                        decremento=decremento+10;
+                        if(500-decremento<0)break;
+                        setSize(500-decremento,500-decremento);
+                        
                          Thread.sleep(100);
                    } catch (InterruptedException ex) {
                       }
@@ -47,9 +45,7 @@ public class Relojito extends javax.swing.JFrame {
         );
         
         t1.start();
-                
     }
-    
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -60,25 +56,17 @@ public class Relojito extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jLabel1.setText("jLabel1");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 20, Short.MAX_VALUE)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGap(0, 400, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 21, Short.MAX_VALUE)
-                .addComponent(jLabel1))
+            .addGap(0, 300, Short.MAX_VALUE)
         );
 
         pack();
@@ -101,25 +89,24 @@ public class Relojito extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Relojito.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Ventanita.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Relojito.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Ventanita.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Relojito.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Ventanita.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Relojito.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Ventanita.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Relojito().setVisible(true);
+                new Ventanita().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
